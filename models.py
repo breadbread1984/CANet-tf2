b30347737b40e793164bc166b4eb42edde0c48f5
breadbread1984 <breadbread1984@163.com>
2020-12-01 13:37:01 +0800

    update

--- models.py
+++ models.py
@@ -63,11 +63,17 @@ def Attention(nshot):
   inputs = tf.keras.Input((None, None, nshot * 512)); # inputs.shape = (qn, h / 16, w / 16, nshot * 512)
   # 1) get masks for query images according to this support image
   outputs = tf.keras.layers.Conv2D(nshot * 256, (3, 3), padding = 'same', groups = nshot)(inputs); # outputs.shape = (qn, h / 16, w / 16, nshot * 256)
+  outputs = tf.keras.layers.BatchNormalization()(outputs); # outputs.shape = (qn, h / 16, w / 16, nshot * 256)
+  outputs = tf.keras.layers.ReLU()(outputs); # outputs.shape = (qn, h / 16, w / 16, nshot * 256)
   outputs = tf.keras.layers.Lambda(lambda x, n: tf.transpose(tf.reshape(x, (tf.shape(x)[0], tf.shape(x)[1], tf.shape(x)[2], n, 256)), (3, 0, 1, 2, 4)), arguments = {'n': nshot})(outputs); # outputs.shape = (nshot, qn, h/16, w/16, 256)
   # 2) get attention weights for query images of this support image
   att = tf.keras.layers.Conv2D(nshot * 256, (3, 3), padding = 'same', groups = nshot)(inputs); # att.shape = (qn, h / 16, w / 16, nshot * 256)
+  att = tf.keras.layers.BatchNormalization()(att); # att.shape = (qn, h / 16, w / 16, nshot * 256)
+  att = tf.keras.layers.ReLU()(att); # att.shape = (qn, h / 16, w / 16, nshot * 256)
   att = tf.keras.layers.MaxPool2D(pool_size = (3, 3), strides = (1, 1), padding = 'same')(att); # att.shape = (qn, h / 16, w / 16, nshot * 256)
   att = tf.keras.layers.Conv2D(nshot * 256, (3, 3), padding = 'same', groups = nshot)(att); # att.shape = (qn, h / 16, w / 16, nshot * 256)
+  att = tf.keras.layers.BatchNormalization()(att); # att.shape = (qn, h / 16, w / 16, nshot * 256)
+  att = tf.keras.layers.ReLU()(att); # att.shape = (qn, h / 16, w / 16, nshot * 256)
   att = tf.keras.layers.Lambda(lambda x, n: tf.transpose(tf.reshape(x, (tf.shape(x)[0], tf.shape(x)[1], tf.shape(x)[2], n, 256)), (3, 0, 1, 2, 4)), arguments = {'n': nshot})(att); # att.shape = (nshot, qn, h / 16, w / 16, 256)
   att = tf.keras.layers.Lambda(lambda x: tf.math.reduce_mean(x, axis = [2, 3, 4], keepdims = True))(att); # att.shape = (nshot, qn, 1, 1, 1)
   attended = tf.keras.layers.Lambda(lambda x: x[0] * x[1])([outputs, att]); # attended.shape = (nshot, qn, h / 16, w / 16, 256)
@@ -85,15 +91,17 @@ def DenseComparisonModule(nshot, pretrain = None):
   block1, block2, block3, block4 = resnet50(imgs_concat); # block2.shape = (nshot + qn, h / 8, w / 8, 512), block3.shape = (nshot + qn, h / 8, w / 8, 1024)
   img_fts = tf.keras.layers.Concatenate(axis = -1)([block2, block3]); # img_fts.shape = (nshot + qn, h / 8, w / 8, 512 + 1024)
   img_fts = tf.keras.layers.Conv2D(256, (3, 3), padding = 'same')(img_fts); # img_fts.shape = (nshot + qn, h / 8, w / 8, 256)
-  supp_fts, qry_fts = tf.keras.layers.Lambda(lambda x: tf.split(x[0], (tf.shape(x[1])[0], tf.shape(x[2])[0]), axis = 0))([img_fts, support, query]); # supp_fts.shape = (nshot, h / 16, w / 16, 256), qry_fts.shape = (qn, h / 16, w / 16, 256)
-  supp_lb = tf.keras.layers.Lambda(lambda x: tf.image.resize(x[0], size = tf.shape(x[1])[1:3], method = tf.image.ResizeMethod.BILINEAR))([labels, img_fts]); # supp_lb.shape = (nshot, h / 16, w / 16, 256)
+  img_fts = tf.keras.layers.BatchNormalization()(img_fts); # img_fts.shape = (nshot + qn, h / 8, w / 8, 256)
+  img_fts = tf.keras.layers.ReLU()(img_fts); # img_fts.shape = (nshot + qn, h / 8, w / 8, 256)
+  supp_fts, qry_fts = tf.keras.layers.Lambda(lambda x: tf.split(x[0], (tf.shape(x[1])[0], tf.shape(x[2])[0]), axis = 0))([img_fts, support, query]); # supp_fts.shape = (nshot, h / 8, w / 8, 256), qry_fts.shape = (qn, h / 8, w / 8, 256)
+  supp_lb = tf.keras.layers.Lambda(lambda x: tf.image.resize(x[0], size = tf.shape(x[1])[1:3], method = tf.image.ResizeMethod.BILINEAR))([labels, img_fts]); # supp_lb.shape = (nshot, h / 8, w / 8, 256)
   proto = tf.keras.layers.Lambda(lambda x: tf.math.reduce_sum(x[0] * x[1], axis = (1,2)) / tf.math.maximum(tf.math.reduce_sum(x[1], axis = (1,2)), 1e-5))([supp_fts, supp_lb]); # proto.shape = (nshot, 256)
-  proto = tf.keras.layers.Lambda(lambda x: tf.tile(tf.reshape(x[0], (tf.shape(x[0])[0], 1, 1, tf.shape(x[0])[1])), (1, tf.shape(x[1])[1], tf.shape(x[1])[2], 1)))([proto, qry_fts]); # proto.shape = (nshot, h / 16, w / 16, 256)
-  qry_comp_fts = tf.keras.layers.Lambda(lambda x: tf.map_fn(lambda y: tf.concat([x[1], tf.tile(tf.expand_dims(y, axis = 0), (tf.shape(x[1])[0], 1, 1, 1))], axis = -1), x[0]))([proto, qry_fts]); # qry_comp_fts.shape = (nshot, qn, h/16, w/16, 512)
-  qry_comp_fts = tf.keras.layers.Lambda(lambda x: tf.reshape(tf.transpose(x, (1,2,3,0,4)), (tf.shape(x)[1], tf.shape(x)[2], tf.shape(x)[3], -1)))(qry_comp_fts); # qry_comp_fts.shape = (qn, h/16, w/16, nshot * 512)
-  qry_comp_fts = Attention(nshot)(qry_comp_fts); # attention.shape = (nshot, qn, h / 16, w / 16, 256)
-  weighted = tf.keras.layers.Softmax(axis = 0)(qry_comp_fts); # outputs.shape = (nshot, qn, h / 16, w / 16, 256)
-  weighted_sum = tf.keras.layers.Lambda(lambda x: tf.math.reduce_sum(x, axis = 0))(weighted); # weighted_sum.shape = (qn, h / 16, w / 16, 256)
+  proto = tf.keras.layers.Lambda(lambda x: tf.tile(tf.reshape(x[0], (tf.shape(x[0])[0], 1, 1, tf.shape(x[0])[1])), (1, tf.shape(x[1])[1], tf.shape(x[1])[2], 1)))([proto, qry_fts]); # proto.shape = (nshot, h / 8, w / 8, 256)
+  qry_comp_fts = tf.keras.layers.Lambda(lambda x: tf.map_fn(lambda y: tf.concat([x[1], tf.tile(tf.expand_dims(y, axis = 0), (tf.shape(x[1])[0], 1, 1, 1))], axis = -1), x[0]))([proto, qry_fts]); # qry_comp_fts.shape = (nshot, qn, h/8, w/8, 512)
+  qry_comp_fts = tf.keras.layers.Lambda(lambda x: tf.reshape(tf.transpose(x, (1,2,3,0,4)), (tf.shape(x)[1], tf.shape(x)[2], tf.shape(x)[3], -1)))(qry_comp_fts); # qry_comp_fts.shape = (qn, h/8, w/8, nshot * 512)
+  qry_comp_fts = Attention(nshot)(qry_comp_fts); # attention.shape = (nshot, qn, h / 8, w / 8, 256)
+  weighted = tf.keras.layers.Softmax(axis = 0)(qry_comp_fts); # outputs.shape = (nshot, qn, h / 8, w / 8, 256)
+  weighted_sum = tf.keras.layers.Lambda(lambda x: tf.math.reduce_sum(x, axis = 0))(weighted); # weighted_sum.shape = (qn, h / 8, w / 8, 256)
   return tf.keras.Model(inputs = (query, support, labels), outputs = weighted_sum);
 
 def AtrousSpatialPyramidPooling(channel):
@@ -131,10 +139,34 @@ def AtrousSpatialPyramidPooling(channel):
   results = tf.keras.layers.ReLU()(results);
   return tf.keras.Model(inputs = inputs, outputs = results);
 
-def IterativeOptimizationModule():
+def IterativeOptimizationModule(has_forward_pass = False):
 
-  inputs = tf.keras.Input((None, None, 256));
-  
+  inputs = tf.keras.Input((None, None, 256)); # inputs.shape = (qn, h / 8, w / 8, 256)
+  if has_forward_pass:
+    mask = tf.keras.Input((None, None, 1)); # mask.shape = (qn, h / 8, w / 8, 1)
+    results = tf.keras.layers.Concatenate(axis = -1)([inputs, mask]); # results.shape = (qn, h / 8, w / 8, 256 + 1)
+  else:
+    results = inputs; # results.shape = (qn, h / 8, w / 8, 256)
+  def make_conv_block(inputs):
+    results = tf.keras.layers.Conv2D(256, (3, 3), padding = 'same')(inputs); # results.shape = (qn, h / 8, w / 8, 256)
+    results = tf.keras.layers.BatchNormalization()(results); # results.shape = (qn, h / 8, w / 8, 256)
+    results = tf.keras.layers.ReLU()(results); # results.shape = (qn, h / 8, w / 8, 256)
+    return results;
+  results = make_conv_block(results); # results.shape = (qn, h / 8, w / 8, 256)
+  results = make_conv_block(results); # results.shape = (qn, h / 8, w / 8, 256)
+  def make_vanilla_residual_block(inputs):
+    residual = inputs;
+    results = tf.keras.layers.Conv2D(256, (3, 3), padding = 'same')(inputs);
+    results = tf.keras.layers.BatchNormalization()(results);
+    results = tf.keras.layers.ReLU()(results);
+    results = tf.keras.layers.Conv2D(256, (3, 3), padding = 'same')(results);
+    results = tf.keras.layers.BatchNormalization()(results);
+    results = tf.keras.layers.Add()([residual, results]);
+    results = tf.keras.layers.ReLU()(results);
+    return results;
+  results = make_vanilla_residual_block(results); # results.shape = (qn, h / 8, w / 8, 256)
+  results = make_vanilla_residual_block(results); # results.shape = (qn, h / 8, w / 8, 256)
+  results = AtrousSpatialPyramidPooling(results); # results.shape = (qn, h / 8, w / 8, 256)
 
 if __name__ == "__main__":
 
@@ -142,7 +174,7 @@ if __name__ == "__main__":
   dcm = DenseComparisonModule(4);
   dcm.save('dcm.h5');
   import numpy as np;
-  query = np.random.normal(size = (1, 224, 224, 3))
+  query = np.random.normal(size = (2, 224, 224, 3))
   support = np.random.normal(size = (4, 224, 224, 3))
   labels = np.random.normal(size = (4, 224, 224, 1))
   results = dcm([query, support, labels]);
